5881b2952dc5cb287ea0e49e54ec89d1d92790d8
Sehaj Chawla <42678107+sehajchawla@users.noreply.github.com>
2018-11-07 11:00:17 +0000

    get coordinates function
    global variables link missing for now

---
+++ IDP/big_file/getCoordinates/getCoordinates.ino
@@ -0,0 +1,19 @@
+#include "header.h";
+void getCoordinates() {
+  // put your setup code here, to run once:
+  bool north = false;
+  float L = 0;
+  float B = 0;
+  float coordinates[1]; //defines array for coordinates
+  if(north == True) {
+    coordinates[0] = getUltrasound()[0];
+    coordinates[1] = getUltrasound()[1];
+  } else {
+    coordinates[0] = L - getUltrasound()[0];
+    coordinates[1] = B - getUltrasound()[1];
+  }
+  
+  
+  return 
+
+}
